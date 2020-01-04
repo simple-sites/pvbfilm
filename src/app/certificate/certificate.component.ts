1d6e7890dfbc2b9752c31c9852c4d09fedec82c5
calidion <calidion@gmail.com>
2020-01-04 15:03:56 +0800

    enabled certificates

--- src/app/certificate/certificate.component.ts
+++ src/app/certificate/certificate.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-certificate',
-  templateUrl: './certificate.component.html',
-  styleUrls: ['./certificate.component.scss']
+  selector: "app-certificate",
+  templateUrl: "./certificate.component.html",
+  styleUrls: ["./certificate.component.scss"]
 })
 export class CertificateComponent implements OnInit {
 
+  certificates = [
+    {
+      url: "/assets/images/zs1.jpg",
+      title: "资质1",
+    },
+    {
+      url: "/assets/images/zs2.jpg",
+      title: "资质2",
+    },
+    {
+      url: "/assets/images/zs4.jpg",
+      title: "资质3",
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
