13e36f016cdf3519a4be7776a429bb7cb6d5ff8e
calidion <calidion@gmail.com>
2020-01-06 19:50:20 +0800

    updated news item

--- src/app/news-item/news-item.component.ts
+++ src/app/news-item/news-item.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-news-item',
-  templateUrl: './news-item.component.html',
-  styleUrls: ['./news-item.component.scss']
+  selector: "app-news-item",
+  templateUrl: "./news-item.component.html",
+  styleUrls: ["./news-item.component.scss"]
 })
 export class NewsItemComponent implements OnInit {
-
+  item = {
+    id: 1,
+    url: "http://localhost:4200/#intro",
+    title: "pvb的应用有那些？",
+    content: "aaa",
+    author: "admin",
+    date: "2013-5-29"
+  };
   constructor() { }
 
   ngOnInit() {
