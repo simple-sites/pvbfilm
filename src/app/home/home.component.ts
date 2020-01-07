64ea4d00422c920e8c967e5729a022d72802f086
calidion <calidion@gmail.com>
2020-01-07 17:56:15 +0800

    enabled home data fetching

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,107 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
-export class HomeComponent {
-  productions = [
-    {
-      image: "assets/images/roadmap/1.jpg",
-      title: "PRODUCTS-T1",
-      intro: "PRODUCTS-T1-INTRO",
-      state: "PRODUCTS-STATE-DONE"
-    },
-    {
-      image: "assets/images/roadmap/2.jpg",
-      title: "PRODUCTS-T2",
-      intro: "PRODUCTS-T2-INTRO",
-      state: "PRODUCTS-STATE-PROGRESSING"
-    },
-    {
-      image: "assets/images/roadmap/3.jpg",
-      title: "PRODUCTS-T3",
-      intro: "PRODUCTS-T3-INTRO",
-      state: "PRODUCTS-STATE-PLANNED"
-    },
-    {
-      image: "assets/images/roadmap/4.jpg",
-      title: "PRODUCTS-T4",
-      intro: "PRODUCTS-T4-INTRO",
-      state: "PRODUCTS-STATE-PLANNED"
-    },
-    {
-      image: "assets/images/roadmap/5.jpg",
-      title: "PRODUCTS-T4",
-      intro: "PRODUCTS-T4-INTRO",
-      state: "PRODUCTS-STATE-PLANNED"
-    }
-  ];
+export class HomeComponent implements OnInit {
+  products = [];
+  news = [];
+  newsUrl = "assets/api/news.json";
+  productUrl = "assets/api/product.json";
 
-  news = [
-    {
-      id: 1,
-      url: "http://localhost:4200/#intro",
-      title: "pvb的应用有那些？",
-      date: "2013-5-29"
-    },
-    {
-      id: 1,
-      url: "http://localhost:4200/#intro",
-      title: "pvb的应用有那些？",
-      date: "2013-5-29"
-    },
-    {
-      id: 1,
-      url: "http://localhost:4200/#intro",
-      title: "pvb的应用有那些？",
-      date: "2013-5-29"
-    },
-    {
-      id: 1,
-      url: "http://localhost:4200/#intro",
-      title: "pvb的应用有那些？",
-      date: "2013-5-29"
-    },
-    {
-      id: 1,
-      url: "http://localhost:4200/#intro",
-      title: "pvb的应用有那些？",
-      date: "2013-5-29"
-    },
-    {
-      id: 1,
-      url: "http://localhost:4200/#intro",
-      title: "pvb的应用有那些？",
-      date: "2013-5-29"
-    },
-    {
-      id: 1,
-      url: "http://localhost:4200/#intro",
-      title: "pvb的应用有那些？",
-      date: "2013-5-29"
-    },
-    {
-      id: 1,
-      url: "http://localhost:4200/#intro",
-      title: "pvb的应用有那些？",
-      date: "2013-5-29"
-    },
-    {
-      id: 1,
-      url: "http://localhost:4200/#intro",
-      title: "pvb的应用有那些？",
-      date: "2013-5-29"
-    },
-    {
-      id: 1,
-      url: "http://localhost:4200/#intro",
-      title: "pvb的应用有那些？",
-      date: "2013-5-29"
-    }
-  ];
-  constructor(
-  ) {
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get(this.newsUrl).subscribe((data: any) => {
+      this.news = data.news;
+    });
+
+    this.http.get(this.productUrl).subscribe((data: any) => {
+      this.products = data.products;
+    });
   }
 }
