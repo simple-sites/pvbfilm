8d95d92a883bae0200847814fcf3162c3b8a99f3
calidion <calidion@gmail.com>
2020-01-07 12:20:09 +0800

    updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,31 +8,31 @@ import { Component } from "@angular/core";
 export class HomeComponent {
   productions = [
     {
-      image: "/assets/images/roadmap/1.jpg",
+      image: "assets/images/roadmap/1.jpg",
       title: "PRODUCTS-T1",
       intro: "PRODUCTS-T1-INTRO",
       state: "PRODUCTS-STATE-DONE"
     },
     {
-      image: "/assets/images/roadmap/2.jpg",
+      image: "assets/images/roadmap/2.jpg",
       title: "PRODUCTS-T2",
       intro: "PRODUCTS-T2-INTRO",
       state: "PRODUCTS-STATE-PROGRESSING"
     },
     {
-      image: "/assets/images/roadmap/3.jpg",
+      image: "assets/images/roadmap/3.jpg",
       title: "PRODUCTS-T3",
       intro: "PRODUCTS-T3-INTRO",
       state: "PRODUCTS-STATE-PLANNED"
     },
     {
-      image: "/assets/images/roadmap/4.jpg",
+      image: "assets/images/roadmap/4.jpg",
       title: "PRODUCTS-T4",
       intro: "PRODUCTS-T4-INTRO",
       state: "PRODUCTS-STATE-PLANNED"
     },
     {
-      image: "/assets/images/roadmap/5.jpg",
+      image: "assets/images/roadmap/5.jpg",
       title: "PRODUCTS-T4",
       intro: "PRODUCTS-T4-INTRO",
       state: "PRODUCTS-STATE-PLANNED"
