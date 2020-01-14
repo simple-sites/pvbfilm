008db851702b844f73565b460b962673224b3638
calidion <calidion@gmail.com>
2020-01-14 16:25:06 +0800

    changed project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'vigcoin'`, () => {
+  it(`should have as title 'qiyebao'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('vigcoin');
+    expect(app.title).toEqual('qiyebao');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to vigcoin!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to qiyebao!');
   });
 });
